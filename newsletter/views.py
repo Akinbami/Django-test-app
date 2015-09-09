2773079ffa0e8effa95fd933dbe6f732a02748f8
Akinbami Dayo <Akinbamizacheus@gmail.com>
2015-09-09 15:54:47 +0100

    The view file in the newsletter has been modified

--- newsletter/views.py
+++ newsletter/views.py
@@ -3,9 +3,22 @@ from .form import SignUpForm
 # Create your views here.
 def home(request):
     title='My Title {0}'.format(request.user)
-    form=SignUpForm()
+    if request.method=='POST':
+        print request.POST
+    form=SignUpForm(request.POST or None)
+    
     context={
         "template_title":title,
         "form":form,
     }
+    
+    if form.is_valid():
+        instance = form.save(commit=False)
+        print instance.email
+        print instance.matric_no
+        instance.save()
+        
+        context={
+            "title":"Thank You",
+        }
     return render(request,'home.html',context)
